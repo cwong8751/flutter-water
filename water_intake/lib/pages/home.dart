906a32698142387e7cba4e5008833c8b46ffebcc
green <kirakbit@gmail.com>
2024-06-06 16:42:05 -0500

    section 11 114

--- water_intake/lib/pages/home.dart
+++ water_intake/lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,36 +11,63 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final amountController = TextEditingController(text: 'Hello');
+
+  void saveWater(String amount) async {
+    final url = Uri.parse('https://water-intaker-default-rtdb.firebaseio.com/water.json');
+
+    var response = await http.post(url, headers: {
+      'Content-Type': 'application/json',
+    }, body: json.encode({
+      'amount': double.parse(amount),
+      'unit': 'ml',
+      'dateTime': DateTime.now().toString()
+    }));
+
+    if (response.statusCode == 200) {
+      print('Data saved');
+      Navigator.of(context).pop(); // Close the dialog after saving data
+    } else {
+      print('Failed to save data');
+    }
+  }
+
   void addWater() {
     showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-              title: Text('Add water'),
-              content: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text('Add water to your daily intake'),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  TextField(
-                    controller: amountController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(), labelText: 'Amount'),
-                  )
-                ],
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Add water'),
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Add water to your daily intake'),
+            SizedBox(height: 10),
+            TextField(
+              controller: amountController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Amount',
               ),
-              actions: [
-                TextButton(onPressed: () {
-                  Navigator.of(context).pop();
-                }, child: Text('Cancel')),
-                TextButton(onPressed: () {
-                  // save data to db
-                }, child: Text('Save')),
-              ],
-            ));
+            ),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Cancel'),
+          ),
+          TextButton(
+            onPressed: () {
+              saveWater(amountController.text);
+            },
+            child: Text('Save'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -53,8 +82,10 @@ class _HomePageState extends State<HomePage> {
         title: Text('Water'),
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
-      floatingActionButton:
-          FloatingActionButton(onPressed: addWater, child: Icon(Icons.add)),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addWater,
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
